9540e1610f926caa7770087df2faf7de9f2ae628
mohammaduzer07 <134217616+mohammaduzer07@users.noreply.github.com>
2024-05-31 16:18:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,61 @@
 //your JS code here. If required.
+let ans = document.getElementById('output');
+
+const promises = [
+	new Promise((resolve)=>{
+		const time = Math.floor(Math.random() * 3+1) * 1000;
+		setTimeout(()=>{
+			resolve({name: "Promise 1", time: time/1000})
+		}, time);
+	}),
+	new Promsise((resolve)=>{
+		const time = Math.floor(Math.random() * 3+1) * 1000;
+		setTimeout(()=>{
+			resolve({name: "Promise 2", time: time / 1000})
+		}, time);
+	}),
+	new Promsise((resolve)=>{
+		const time = Math.floor(Math.random() * 3+1) * 1000;
+		setTimeout(()=>{
+			resolve({name: "Promise 2", time: time / 1000})
+		}, time);
+	}),
+];
+
+async function callFunc(){
+	const start = new Date();
+
+	res.innerHTML += `
+		<tr id="loading">
+			<td colspan=2>Loading...</td>
+		</tr>
+		`;
+	await Promise.all(promises)
+	.then((results)=>{
+		res.innerHTML = ``;
+		results.forEach((e)=>{
+			res.innerHTML += `
+				<tr>
+					<td>${e.name}<td>
+					<td>${e.time}<td>
+				</tr>
+				`;
+		});
+	})
+	.catch((error)=>{
+		console.log(error);
+	});
+
+	const end = new Date();
+	let timeInmilisec = end-start;
+
+	res.innerHTML += `
+		<tr>
+			<td>Total</td>
+			<td>${timeInmilisec / 1000}</td>
+		</tr>
+		`;
+}
+
+callFunc();
+
